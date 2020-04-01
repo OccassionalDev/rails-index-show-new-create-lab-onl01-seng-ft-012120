7680ba1c60661a715716c5166019481e6335cb78
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-01 03:28:46 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -13,5 +13,8 @@ class CouponsController < ApplicationController
   
   def create 
     @coupon = Coupon.new 
-    @coupon.coupon_code = 
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save 
+  end 
 end 
